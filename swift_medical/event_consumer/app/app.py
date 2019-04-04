9b4fc3166dbb12cf59ee14dfa69fb53f94c0a977
Harnek Sidhu <harnek.sidhu@traderev.com>
2019-04-04 15:27:35 -0400

    Backup

--- swift_medical/event_consumer/app/app.py
+++ swift_medical/event_consumer/app/app.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python
 from kafka import KafkaConsumer
 
-class Consumer():
-  def __init__(self):
-    print("starting")
-    consumer = KafkaConsumer('messages', bootstrap_servers=['kafka:9092'],
-                             auto_offset_reset='latest',
-                             enable_auto_commit=True,
-                             auto_commit_interval_ms=500,
-                             consumer_timeout_ms=3000)
-    for message in consumer:
-      print ("Consumed Msg -> '%s' on Topic -> '%s' with Offset -> %d" %
-                    (message.value.decode('utf-8'), message.topic, message.offset))
-    consumer.close()
+consumer = KafkaConsumer('messages', group_id='event_consumer')
 
-Consumer()
+while True:
+  for msg in consumer:
+    print (msg)
+consumer.close()
