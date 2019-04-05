3c7e6e85aed9c40de23f9b5ff9c4b74e1712ffc0
Harnek Sidhu <harnek.sidhu@traderev.com>
2019-04-05 00:12:39 -0400

    Backup

--- swift_medical/tmp/app/main/util/kafka.py
+++ swift_medical/tmp/app/main/util/kafka.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 from kafka import KafkaProducer
+import json
 
 class Kafka():
   def __init__(self):
-    print("starting")
-    producer = KafkaProducer(bootstrap_servers=['kafka:9092'], retries=5)
-    producer.send('messages', b'super-duper-message')
-    print("Published msg -> 'super-duper-message' on Topic -> 'messages'")
+    self.producer = KafkaProducer(bootstrap_servers=['kafka:9092'], 
+                                  retries=5,
+                                  value_serializer=lambda m: json.dumps(m).encode('ascii'))
+
+  def publish_event(self, topic, payload):
+    self.producer.send(topic, payload)
     # block until all async messages are sent
     producer.flush()
     # tidy up the producer connection
     producer.close()
-
-Producer()
-while True:
-  print("hi")
