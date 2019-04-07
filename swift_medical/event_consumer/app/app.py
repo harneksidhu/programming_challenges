6fa3ac74fd90a6e61175f405e8f061a82d3f9cb3
Harnek Sidhu <harnek.sidhu@traderev.com>
2019-04-07 16:29:58 -0400

    Backup

--- swift_medical/event_consumer/app/app.py
+++ swift_medical/event_consumer/app/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer('messages', group_id='event_consumer')
+consumer = KafkaConsumer('messages', group_id='event_consumer', enable_auto_commit=False)
 
 while True:
   for msg in consumer:
     print (msg)
-consumer.close()
+consumer.close(autocommit=False)
