a32650d9e4cfe365c1a3677529c999e3db884cf6
Harnek Sidhu <harnek.sidhu@traderev.com>
2019-04-07 18:36:09 -0400

    Backup

--- swift_medical/event_consumer/app/app.py
+++ swift_medical/event_consumer/app/app.py
@@ -1,9 +1,21 @@
-#!/usr/bin/env python
 from kafka import KafkaConsumer
+import json
+import requests
 
-consumer = KafkaConsumer('messages', group_id='event_consumer', enable_auto_commit=False)
+def store_event_to_event_db(payload):
+  r = requests.post('http://event-api:5000/events', data=payload)
+  r.raise_for_status()
 
+topics = ['start','goal', 'pass', 'save', 'end']
+consumer = KafkaConsumer(bootstrap_servers=['kafka:9092'], group_id='event_consumer', enable_auto_commit=False, value_deserializer=lambda m: json.loads(m.decode('ascii')))
+consumer.subscribe(topics)
+print("Starting Consumer")
 while True:
   for msg in consumer:
-    print (msg)
+    print("Message received")
+    print(msg)
+    store_event_to_event_db(msg.value[0])
+    print("commit offset")
+    consumer.commit()
+    print("offset comitted")
 consumer.close(autocommit=False)
