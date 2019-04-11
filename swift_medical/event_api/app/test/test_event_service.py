ec4ef8825f8024d17429be6a2b0a6667310c936a
Harnek Sidhu <harnek.sidhu@traderev.com>
2019-04-11 19:18:52 -0400

    Unit testing

---
+++ swift_medical/event_api/app/test/test_event_service.py
@@ -0,0 +1,65 @@
+import unittest
+import datetime
+from app.main.service.event_service import export_event, save_event
+from app.main.model.event import FifaEvent
+from app.test.base import BaseTestCase
+from app.main import db
+
+class TestExportEvent(BaseTestCase):
+
+  def test_missing_format(self):
+    payload = {
+      'event_id': '061371f1'
+    }
+    response = export_event(payload)
+    self.assertEquals(response.json, dict(error='bad request'))
+    self.assertEquals(response._status_code, 400)
+
+  def test_missing_event(self):
+    payload = {
+      'Format': 'yaml'
+    }
+    response = export_event(payload)
+    self.assertEquals(response.json, dict(error='bad request'))
+    self.assertEquals(response._status_code, 400)
+
+  def test_incorrect_format(self):
+    payload = {
+      'event_id': '061371f1',
+      'Format': 'incorrect'
+    }
+    response = export_event(payload)
+    self.assertEquals(response.json, dict(error='bad request'))
+    self.assertEquals(response._status_code, 400)
+
+  def test_event_not_found(self):
+    payload = {
+      'event_id': '061371f1',
+      'Format': 'yaml'
+    }
+    response = export_event(payload)
+    self.assertEquals(response.json, dict(error='event not found'))
+    self.assertEquals(response._status_code, 404)
+
+  def test_event_found(self):
+    payload = {
+      'event_id': '061371f1-eda5-4fea-96ee-436a6dd4f8d7',
+      'Format': 'yaml'
+    }
+    event = FifaEvent(
+      message_id='061371f1-eda5-4fea-96ee-436a6dd4f8d7',
+      match_id='ef4146ee-64e3-430b-b6af-b12671e4beef',
+      message_at=datetime.datetime(2019, 4, 11, 23, 16, 48, 247113),
+      event_at=datetime.datetime(2019, 4, 11, 23, 16, 48, 247113),
+      event_type='start',
+      location='toronto',
+      team_1='toronto',
+      team_2='montreal'
+    )
+    db.session.add(event)
+    db.session.commit()
+    response = export_event(payload)
+    self.assertEquals(response._status_code, 200)
+
+if __name__ == '__main__':
+  unittest.main()
